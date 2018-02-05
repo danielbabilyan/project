b0a42ab1531fc696e0cc7ae01a94f6b1883adf71
Daniel Babilyan <daniel0585581234@gmail.com>
2018-02-05 14:10:23 +0200

    test1

--- fronted/server.js
+++ fronted/server.js
@@ -3,7 +3,10 @@ var path = require('path');
 var app = express();
 var port = process.env.PORT || 1111;
 
+app.use('/images/', express.static(path.join(__dirname, './global_files/images')));
+
 app.use('/dashboard', express.static(path.join(__dirname, './dashboard/public')));
+
 app.get('/dashboard*', function(req, res) {
     res.sendfile('./dashboard/public/index.html');
 });
